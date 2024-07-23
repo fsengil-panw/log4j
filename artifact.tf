eee7f8f2e96ea6fb5ae1b693d7be563b33833400
Fulya Sengil <92736005+fsengil-panw@users.noreply.github.com>
2024-07-23 21:28:43 +0100

    Create artifact.tf

---
+++ artifact.tf
@@ -0,0 +1,20 @@
+resource "google_artifact_registry_repository" "test-repo" {
+  location = "us-central1"
+  repository_id = "test-repo"
+  description = "Test Repository"
+  format = "DOCKER"
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/artifactregistry.reader"
+    members = [
+      "user:example@gmail.com"
+    ]
+  }
+}
+
+resource "google_artifact_registry_repository_iam_policy" "policy" {
+  repository = google_artifact_registry_repository.demo_repo.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
